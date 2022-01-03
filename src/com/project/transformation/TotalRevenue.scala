a9a3257f3ef139130de537ca9fe8ceaff9144d16
Kumaran1508 <43463804+Kumaran1508@users.noreply.github.com>
2022-01-03 22:38:54 +0530

    Update TotalRevenue.scala

--- src/com/project/transformation/TotalRevenue.scala
+++ src/com/project/transformation/TotalRevenue.scala
@@ -9,22 +9,38 @@ import java.nio.charset.StandardCharsets
 import java.nio.file.{Files, Paths}
 
 class TotalRevenue extends UtilityLoader with TotalRevenue{
+  /**
+   * @author Kumaran
+   *         kumarans1508@gmail.com
+   *         
+   * Extracts Revenue of Retailer France        
+   * @param RetailerCountry
+   */
   override def queryCreator(RetailerCountry: RevenueEnum.Value): Unit = {
-    //Extraction of Revenue of Retailer France
     val totalRevenue = sparkSession.sql("SELECT Revenue FROM SALES_TABLE WHERE RetailerCountry='"+RetailerCountry+"'")
 
     calculateRevenue(totalRevenue)
   }
 
+  /**
+   *
+   * @param revenues
+   * calculates total revenue of the country France
+   *
+   */
   override def calculateRevenue(revenues : DataFrame): Unit ={
-    //calculating total revenue of the country France
     val total = revenues.agg(sum("Revenue").cast("long")).first.getLong(0)
     println("Total revenue of the Retailer Country France==>"+total)
     writeToFile(total)
   }
 
+  /**
+   *
+   * @param total
+   * Writes the result to a text file
+   *
+   */
   override def writeToFile(total:Long): Unit ={
-    // Writing to a text file
     Files.write(Paths.get("TotalRevenue.txt"), total.toString.getBytes(StandardCharsets.UTF_8))
   }
 
