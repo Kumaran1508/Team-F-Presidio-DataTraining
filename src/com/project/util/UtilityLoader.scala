69f7272b5bdc24222ca0b05ef4f9c51ae8532e66
Priya-2001 <63697123+Priya-2001@users.noreply.github.com>
2022-01-03 09:07:47 +0530

    Update UtilityLoader.scala

--- src/com/project/util/UtilityLoader.scala
+++ src/com/project/util/UtilityLoader.scala
@@ -1,21 +1,18 @@
 package com.project.util
 import org.apache.spark.sql.SparkSession
 import scala.io.Source
-trait UtilityLoader {
-
-    //Loading winutils
-    System.setProperty("hadoop.home.dir", "D:\\winutils\\")
+class UtilityLoader {
 
     //Creation of Spark Session
     val sparkSession = SparkSession.builder()
-      .appName("CodaDataJOB")
-      .master("local[*]")
+      .appName("SalesDataAnalysis")
+      .master("spark://priya:7077")
       .getOrCreate()
 
     //Reading CSV
     import sparkSession.implicits._
     val csvRead=for {
-        line <- Source.fromFile("sales.csv").getLines().drop(1).toVector
+        line <- Source.fromFile("/home/priya/Downloads/sales.csv").getLines().drop(1).toVector
         values = line.split(",")
     } yield Salesdata(values(0), values(1), values(2), values(3), values(4),values(5),values(6),values(7),values(8),values(9),values(10),values(11),values(12),values(13))
     val csvDF = csvRead.toDF()
