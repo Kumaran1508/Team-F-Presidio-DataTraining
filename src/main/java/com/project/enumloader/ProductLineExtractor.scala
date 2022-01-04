c1bbe87e2efd038d19090486c02eb3041ee12b5c
Priya-2001 <63697123+Priya-2001@users.noreply.github.com>
2022-01-04 08:17:57 +0530

    Update ProductLineExtractor.scala

--- src/main/java/com/project/enumloader/ProductLineExtractor.scala
+++ src/main/java/com/project/enumloader/ProductLineExtractor.scala
@@ -1,5 +1,8 @@
 package com.project.enumloader
 
+/**
+ * ProductLineExtractor is effectuated by ProductTypeCounter for obtaining the ProductLine
+ */
 object ProductLineExtractor extends Enumeration{
   type ProductLineExtractor = Value
   val ProductLine = Value("Golf Equipment")
