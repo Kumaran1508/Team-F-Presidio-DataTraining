af198d52fe69c8896a570d62925267e65182fdd4
Priya-2001 <63697123+Priya-2001@users.noreply.github.com>
2022-01-04 08:18:59 +0530

    Update RetailerCountryExtractor.scala

--- src/main/java/com/project/enumloader/RetailerCountryExtractor.scala
+++ src/main/java/com/project/enumloader/RetailerCountryExtractor.scala
@@ -1,5 +1,8 @@
 package com.project.enumloader
 
+/**
+ * RetailerCountryExtractor is effectuated by TotalRevenueCalculator for obtaining the Retailer Country
+ */
 object RetailerCountryExtractor extends Enumeration{
   type RetailerCountryExtractor = Value
   val retailerCountry = Value("France")
